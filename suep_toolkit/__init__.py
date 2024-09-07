28c21036c0f1805bdaac5792f59d1435c2a9ebb2
zhengxyz123 <3422794023@qq.com>
2024-09-07 21:51:23 +0800

    Add a pyproject.toml.

--- suep_toolkit/__init__.py
+++ suep_toolkit/__init__.py
@@ -20,6 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+"""A toolkit for students at Shanghai University of Electric Power.
+"""
+
 version = "0.1.0"
 __version__ = version
 
