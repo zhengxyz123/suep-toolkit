9023694b9fbc3f3f61df7b659596119cc096c1a2
zhengxyz123 <3422794023@qq.com>
2024-12-08 15:20:35 +0800

    Finish selecting courses.

---
+++ examples/elect_course.py
@@ -0,0 +1,103 @@
+# suep-toolkit, A toolkit for students at Shanghai University of Electric Power.
+#
+# Copyright (c) 2024 zhengxyz123
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# 这是一个自动化选课脚本，帮助你快速选到心仪的课程。
+# 你唯一需要做的就是以命令行参数传递一个选课列表文件，然后根据程序的提示输入一些信息。
+# 选课列表文件的每一行都是一个教务系统上的课程序号（形如“xxxxxxx.xx”，其中“x”代表一位数字）。
+
+import getpass
+import os
+import sys
+from pathlib import Path
+
+from suep_toolkit import auth
+from suep_toolkit import course as course_system
+from suep_toolkit.util import AuthServiceError, VPNError
+
+
+def main(courses_file: Path) -> int:
+    if not courses_file.exists():
+        print(f"文件'{courses_file}'不存在")
+        return 1
+    if "SUEP_USERNAME" in os.environ and "SUEP_PASSWORD" in os.environ:
+        service = auth.AuthService(
+            os.environ["SUEP_USERNAME"], os.environ["SUEP_PASSWORD"]
+        )
+    else:
+        service = auth.AuthService(input("用户名: "), getpass.getpass("密码: "))
+    if service.need_captcha():
+        with open("captcha.jpg", "wb") as f:
+            f.write(service.get_captcha_image())
+        service.set_captcha_code(input("验证码: "))
+        os.remove("captcha.jpg")
+    try:
+        service.login()
+    except AuthServiceError:
+        print("登陆失败")
+        return 1
+    try:
+        course_mgr = course_system.CourseManagement(service.session)
+    except VPNError:
+        print("需要先启动 VPN")
+        return 1
+    try:
+        electable_course = list(course_mgr.electable_course)
+    except:
+        print("读取课程列表失败, 请重试")
+        return 1
+    wanted_list = []
+    success_list = []
+    for line in courses_file.read_text().splitlines():
+        course_no = line.strip()
+        for course in electable_course:
+            if course._no == course_no:
+                wanted_list.append(course)
+    print("已选课程:")
+    for course in wanted_list:
+        print(f"{course._no} - {course.name}")
+    print("按下回车开始选课...")
+    input()
+    while len(success_list) != len(wanted_list):
+        for course in wanted_list:
+            if course.id in success_list:
+                continue
+            try:
+                course.elect()
+                print(f"{course.name}: 选课成功")
+                success_list.append(course.id)
+            except course_system.ElectCourseError as error:
+                if "已经选过" in error.error:
+                    success_list.append(course.id)
+                print(f"{course.name}: {error.error}")
+            except KeyboardInterrupt:
+                return 0
+            except:
+                return 0
+    return 0
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("需要提供一个选课列表文件")
+        exit(1)
+    else:
+        exit(main(Path(sys.argv[1])))
--- suep_toolkit/course.py
+++ suep_toolkit/course.py
@@ -22,21 +22,27 @@
 
 import json
 import re
+import socket
+from typing import Iterable
 
 import requests
 from bs4 import BeautifulSoup
 
-from suep_toolkit.util import AuthServiceError, VPNError, test_network
+from suep_toolkit.util import AuthServiceError, VPNError
 
 
 class ElectCourseError(Exception):
     """选课失败时引发此异常。"""
 
-    pass
+    def __init__(self, error: str) -> None:
+        super().__init__(error)
+        self.error = error
 
 
 class Course:
     """一个选课类。"""
+
+    operator_url = "https://jw.shiep.edu.cn/eams/stdElectCourse!batchOperator.action"
 
     def __init__(
         self,
@@ -44,17 +50,47 @@ class Course:
         course_name: str,
         course_id: int,
         course_no: str,
+        profile_id: str,
     ) -> None:
         self._session = session
         self._name = course_name
         self._id = course_id
         self._no = course_no
+        self._profile_id = profile_id
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._name!r}, id={self._id!r}, no={self._no!r})"
 
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def no(self) -> str:
+        return self._no
+
     def elect(self) -> None:
-        pass
+        response = self._session.post(
+            self.operator_url,
+            params={"profileId": self._profile_id},
+            data={"optype": "true", "operator0": f"{self._id}:true:0"},
+        )
+        response.raise_for_status()
+
+        dom = BeautifulSoup(response.text, features="html.parser")
+        try:
+            result = dom.select("table>tr>td>div")[0].text.strip()
+        except:
+            raise ElectCourseError("其它错误")
+        if any([s in result for s in ["失败", "内部错误", "过快点击"]]):
+            raise ElectCourseError(result)
+
+    def cancel(self) -> None:
+        raise NotImplementedError("not implemented")
 
 
 class CourseManagement:
@@ -71,10 +107,13 @@ class CourseManagement:
 
     def __init__(self, session: requests.Session) -> None:
         self._session = session
-        if not test_network():
+        self._course_list: list[Course] = []
+        try:
+            socket.create_connection(("jw.shiep.edu.cn", 443), timeout=0.5)
+        except TimeoutError as error:
             raise VPNError(
                 "you are not connected to the campus network, please turn on vpn"
-            )
+            ) from error
         response = self._session.get(self.login_url)
         response.raise_for_status()
         dom = BeautifulSoup(response.text, features="html.parser")
@@ -84,14 +123,13 @@ class CourseManagement:
 
         self._session.get(self.course_table1_url).raise_for_status()
 
-    @property
-    def electable_course(self):
+    def _get_course_list(self) -> None:
         response = self._session.get(self.elect_course1_url)
         response.raise_for_status()
         for profile_id in re.finditer(r"electionProfile.id=(\d+)", response.text):
             response = self._session.get(
                 self.elect_course2_url,
-                params={"electionProfile.id": profile_id.group(1)},
+                params={"electionProfile.id": profile_id.group(1)}
             )
             response.raise_for_status()
             if "不在选课时间内" in response.text:
@@ -99,11 +137,26 @@ class CourseManagement:
             response = self._session.get(
                 self.course_data_url, params={"profileId": profile_id.group(1)}
             )
-            legal_json = re.sub(r"(,|{)(\w+):", r'\1"\2":', response.text[18:-1])
-            legal_json = legal_json.replace("'", '"')
-            course_json = json.loads(legal_json)
-            for course in course_json:
-                yield Course(self._session, course["name"], course["id"], course["no"])
+            legal_json_str = re.sub(
+                r"(,|{)(\w+):", r'\1"\2":', response.text[18:-1]
+            ).replace("'", '"')
+            legal_json = json.loads(legal_json_str)
+            for course in legal_json:
+                self._course_list.append(
+                    Course(
+                        self._session,
+                        course["name"],
+                        course["id"],
+                        course["no"],
+                        profile_id.group(1),
+                    )
+                )
+
+    @property
+    def electable_course(self) -> Iterable[Course]:
+        if len(self._course_list) == 0:
+            self._get_course_list()
+        yield from self._course_list
 
 
 __all__ = ("CourseManagement",)
